cbceb4e5dfe8fdefd8ef9bb1aba97e4694080bd5
creativityfingers <156340011+creativityfingers@users.noreply.github.com>
2024-01-11 17:13:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+document.querySelector(".bttn").addEventListener("click", () => {
+    document.querySelector(".bars-menu").classList.toggle("active");
+  });
+  
+
+  document.querySelectorAll(".bars-menu").forEach((item) => {
+    item.addEventListener("click", () => {
+      document.querySelector(".bars-menu").classList.toggle("active");
+    });
+  });
+
